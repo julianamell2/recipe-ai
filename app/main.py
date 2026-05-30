731b36e68ded6189efc9b2ef9ea2d3a3b7eac163
Julian Amell <amellromeroj@gmail.com>
2026-05-29 19:19:20 -0500

    Implementacion de  JWT authentication y rutas protegidas

--- app/main.py
+++ app/main.py
@@ -6,6 +6,10 @@ from app.database import engine
 from app.models.user import User
 from app.routers.auth import router as auth_router
 
+#Prueba temporalmente
+from app.security import get_current_user
+from fastapi import Depends
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
@@ -17,3 +21,14 @@ app.include_router(auth_router)
 @app.get("/")
 def root():
     return {"message": "Recipe AI funcionando"}
+
+#enpoint prueba
+@app.get("/me")
+def me(
+    current_user: User = Depends(get_current_user)
+):
+    return {
+        "id": current_user.id,
+        "nombre": current_user.nombre,
+        "email": current_user.email
+    }
--- app/security.py
+++ app/security.py
@@ -1,4 +1,11 @@
 from passlib.context import CryptContext
+from jose import JWTError
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+
+from app.database import get_db
+from app.models.user import User
 
 pwd_context = CryptContext(
     schemes=["bcrypt"],
@@ -22,7 +29,9 @@ load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
-
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="/auth/login"
+)
 
 def create_access_token(data: dict):
     to_encode = data.copy()
@@ -36,3 +45,39 @@ def create_access_token(data: dict):
         SECRET_KEY,
         algorithm=ALGORITHM
     )
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+):
+
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail="Could not validate credentials"
+    )
+
+    try:
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+
+        user_id = payload.get("sub")
+
+        if user_id is None:
+            raise credentials_exception
+
+    except JWTError:
+        raise credentials_exception
+
+    user = (
+        db.query(User)
+        .filter(User.id == int(user_id))
+        .first()
+    )
+
+    if user is None:
+        raise credentials_exception
+
+    return user
